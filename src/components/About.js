02190ab1b2932cbb1e1be6b5fc679c2e4396d035
Priyansh Rastogi <priyansh.rastogi54@gmail.com>
2019-07-15 21:58:10 +0530

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
       <h2><span className='topic-highlight'># about</span></h2>
       <div className='content'>
         {/*<p>Based in New Delhi, India. Graduated in 2019 with Major in Computer Science. Passionate about building things. Hungry for learning new things. Loves to code. Builds apps for Web, Mobile, and Desktop. Javascript Fanatic. Pythonista. Javaier. Experiencing a variety of food and drinks. Find him listening to Guns N' Roses, Eagles, Imagine Dragon, Coldplay or EDM when not coding or sleeping. Occasionally writes things on his blog.</p>*/}
-        <p>Based in Gurgaon, India. I am a Software Engineer at American Express. Passionate about building things. Hungry for learning new things. Loves to code. Builds apps for Web, Mobile, and Desktop. Javascript Fanatic. Pythonista. Javaier.</p>
+        <p>Software Engineer at American Express, based in Gurgaon, India. Passionate about building things. Loves to code. Builds apps for Web, Mobile, and Desktop. Javascript Fanatic. Pythonista. Javaier. Find him listening to Guns N' Roses, Eagles, Imagine Dragon, Coldplay, EDM or binge watching some TV show when not coding or sleeping. Occasionally writes things on his blog.</p>
       </div>
     </div>
   )
