28c3bcbbe664c7bc0613351495cbaecb29b620ed
Priyansh Rastogi <priyansh.rastogi54@gmail.com>
2021-04-18 12:45:02 +0530

    Added perosnal finance redirect

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/personalfinance',
+        destination: 'https://blog.priyanshrastogi.com/how-to-not-lose-your-money-personal-finance-for-millennials-part-1-d9185ac1a5a4',
+        permanent: true,
+      },
+    ]
+  },
+}
