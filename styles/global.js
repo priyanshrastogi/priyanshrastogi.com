4be2f5cf8a1e40fe93b05bec348761a264f59f95
Priyansh Rastogi <priyansh.rastogi54@gmail.com>
2022-03-13 15:41:16 +0530

    remove unused code

--- styles/global.js
+++ styles/global.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Global, css } from '@emotion/react';
 
-const lightStyles = css`
+const styles = css`
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
   body {
     margin: 0px;
     font-family: 'Inter', sans-serif;
   }
-
-  p {
-    margin: 0px;
-  }
 `;
 
 const GlobalStyle = () => {
-  return <Global styles={lightStyles} />
+  return <Global styles={styles} />
 };
 
 export default GlobalStyle;
